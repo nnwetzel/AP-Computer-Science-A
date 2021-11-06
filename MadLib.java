c9ca83d3cc0acc26c38e681b5a0421e6dd105fdd
Nat Wetzel <50302468+nnwetzel@users.noreply.github.com>
2021-11-05 19:52:07 -0700

    Updated MadLib project

--- MadLib.java
+++ MadLib.java
@@ -2,37 +2,26 @@ import java.util.Scanner;
 
 class MadLib {
 
-   String r1;
-   String r2;
-   String r3;
-   String r4;
-   String r5;
-   String madlib1;
-   
+   String madlibTemplate;
+
    /*
-      Prompts the user to enter five responses, setting them as variables.
-      @param the user's input
+      Receives the user's input as variables and replaces the specifiers to display the completed madlib.
+      @param   fillIn1 - the user's first response
+      @param   fillIn2 - the user's second response
+      @param   fillIn3 - the user's third response
+      @param   fillIn4 - the user's fourth response
+      @param   fillIn5 - the user's fifth response
    **/
-   MadLib() {
-      Scanner MadLib = new Scanner(System.in);
-      
-      System.out.println("Enter a noun:");
-      r1 = MadLib.nextLine();
-      System.out.println("Enter a past-tense verb:");
-      r2 = MadLib.nextLine();
-      System.out.println("Enter a name:");
-      r3 = MadLib.nextLine();
-      System.out.println("Enter a vehicle:");
-      r4 = MadLib.nextLine();
-      System.out.println("Enter a food:");
-      r5 = MadLib.nextLine(); 
+   MadLib(String fillIn1, String fillIn2, String fillIn3, String fillIn4, String fillIn5) {
+
+      madlibTemplate = String.format("I went to school with my %s, however my teacher said it wasn't allowed so I %s her and ran out of the classroom. I called\n%s. When they arrived, I got into their %s and they drove me home. Once I arrived, I ran inside and grabbed some %s out\nof the refridgerator.", fillIn1, fillIn2, fillIn3, fillIn4, fillIn5);
    } 
-   
+
    /*
-      Returns the madlib after replacing the format specifiers with variables.
-      @return the madlib
+      Returns the completed madlib.
+      @return  the completed madlib
    **/
    public String toString() {
-      return madlib1 = String.format("I went to school with my %s, however my teacher said it wasn't allowed so I %s her and ran out of the classroom. I called\n%s. When they arrived, I got into their %s and they drove me home. Once I arrived, I ran inside and grabbed some %s out\nof the refridgerator.", r1, r2, r3, r4, r5);
+      return madlibTemplate;
    } 
 }
--- MadLibMain.java
+++ MadLibMain.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class MadLibMain {
 
    public static void main( String args[] ) {
-      // Constructs MadLib object
-      MadLib madlib = new MadLib();
-      
-      // Prints completed madlib
-      System.out.println(madlib.toString());
+
+      Scanner systemInput = new Scanner(System.in);
+
+      System.out.println("Enter a noun:");
+      String fillIn1 = systemInput.nextLine();
+      System.out.println("Enter a past-tense verb:");
+      String fillIn2 = systemInput.nextLine();
+      System.out.println("Enter a name:");
+      String fillIn3 = systemInput.nextLine();
+      System.out.println("Enter a vehicle:");
+      String fillIn4 = systemInput.nextLine();
+      System.out.println("Enter a food:");
+      String fillIn5 = systemInput.nextLine(); 
+
+      MadLib madlibObj = new MadLib(fillIn1, fillIn2, fillIn3, fillIn4, fillIn5);
+
+      System.out.println(madlibObject.toString());
    }
 }
