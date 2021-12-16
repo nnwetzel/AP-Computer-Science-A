7e64a599ed827f5a9273595921675fa67a14fc60
Nat Wetzel <50302468+nnwetzel@users.noreply.github.com>
2021-12-16 10:27:42 -0800

    Updated FracCalc

--- Fraction Calculator/FracCalc.java
+++ Fraction Calculator/FracCalc.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class FracCalc {
 	
@@ -20,9 +20,9 @@ public class FracCalc {
       	// Get the input from the user
          System.out.println("Please enter an equation that you want solved.");
          System.out.println("For example, \"2_1/3 + 1_1/5\".");
-
+      
          String userInput = input.nextLine();
-
+      
          if (userInput.equalsIgnoreCase("quit") )
          {
             done = true;
@@ -33,58 +33,50 @@ public class FracCalc {
             System.out.println(result.toString());
          }
       	
-      // Exit condition met?  If not go around again!
       } while (!done);
    	
       input.close();
    	
-   } // end main
-	
-   public static Fraction calculate(String input) {
+   } 
    	
-   	// a fraction object to hold the resultant fraction
+   public static Fraction calculate(String input) {
       
       Fraction result = null;
-   	
-   	// TODO: Split the user input into the components (f1 op f2)
    
       input = input.trim().replaceAll(" +", " ");
-   
+      
       String[] parts = input.split(" ");
       
       Fraction frac1 = new Fraction(parts[0]);
       String operand = parts[1];
       Fraction frac2 = new Fraction(parts[2]);
-   	
-   	// TODO: Do the calculation based on the operator
+      
       int num = 0;
       int den = 0;
       
-      if (operand.contains("+")) {
-      
+      if (operand.charAt(0) == '+') {
+         
          num = (frac1.getNum() * frac2.getDen()) + (frac2.getNum() * frac1.getDen());
          den = frac1.getDen() * frac2.getDen();
       }
-      if (operand.contains("-")) {
-      
+      if (operand.charAt(0) == '-') {
+         
          num = (frac1.getNum() * frac2.getDen()) - (frac2.getNum() * frac1.getDen());
          den = frac1.getDen() * frac2.getDen();
       }
-      if (operand.contains("*")) {
-      
+      if (operand.charAt(0) == '*') {
+         
          num = frac1.getNum() * frac2.getNum();
          den = frac1.getDen() * frac2.getDen();
       }
-      if (operand.contains("/")) {
-      
+      if (operand.charAt(0) == '/') {
+         
          num = frac1.getNum() * frac2.getDen();
          den = frac2.getNum() * frac1.getDen();
       }
-   	// TODO: Create the resultant Fraction and return it
+      
       result = new Fraction(num, den);
-   	
+   
       return result;
-   	
-   } // end calculate
-	
-} // end FracCalc class
+   }
+}
--- Fraction Calculator/Fraction.java
+++ Fraction Calculator/Fraction.java
@@ -1,45 +1,51 @@
-import java.util.*;
-
 public class Fraction {
    
    private int numerator;
    private int denominator;
-   private int whole;
 
+   /**
+   
+   
+   */
    public Fraction(String fraction) {
-      // TODO: parse the fraction into the numerator and denominator
    
       if (fraction.contains("_") && fraction.contains("/")) { // Parses the fraction if it is a mixed number.
       
-         whole = Integer.parseInt(fraction.substring(0, fraction.indexOf('_')));
+         int whole = Integer.parseInt(fraction.substring(0, fraction.indexOf('_')));
          numerator = Integer.parseInt(fraction.substring(fraction.indexOf('_') + 1, fraction.indexOf('/')));
          denominator = Integer.parseInt(fraction.substring(fraction.indexOf('/') + 1));
+         
          numerator += Math.abs(whole) * denominator;
          
          if (whole < 0) {
-         
+            
             numerator *= -1;
+            
          }
       }
       else if (fraction.contains("/")) { // Parses the fraction.
-      
+         
          numerator = Integer.parseInt(fraction.substring(0, fraction.indexOf('/')));
          denominator = Integer.parseInt(fraction.substring(fraction.indexOf('/') + 1));
+         
+         if (numerator > 0 && denominator < 0) {
+            
+            numerator *= -1;
+            denominator = Math.abs(denominator);
+         }
       }
       else { // Parses the fraction if it is a whole number.
-      
          numerator = Integer.parseInt(fraction);
-         denominator = 1; 
+         denominator = 1;
       }
-
-   } // end Fraction constructor
+   
+   }
 
    public Fraction(int numerator, int denominator) {
-      // TODO: store the numerator and denominator sent in into this object's data
-
+   
       this.numerator = numerator;
       this.denominator = denominator;
-   } // end Fraction constructor
+   }
   
    public int getNum() { // Returns numerator.
    
@@ -52,12 +58,9 @@ public class Fraction {
    }
    
    public String toString() {
-      
-      // The string that stores the formatted output for this class when printed
    
+      int whole = 0;
       String output = "";
-
-      // TODO: make this reduce the fraction and create the string to return
    
       for (int i = Math.abs(numerator); i >= 1; i--) { // Finds the GCD of two fractions and reduces them.
       
@@ -92,34 +95,21 @@ public class Fraction {
             whole = numerator / denominator;
             numerator %= denominator;
          }
-         
          output = whole + "_" + numerator + "/" + denominator; 
       }
       else if (Math.abs(numerator) < Math.abs(denominator)) { // Formats the fraction if it is not improper.
-      
-         if (denominator < 0) { // If the numerator or denominator is negative, transforms the fraction accordingly.
-         
-            numerator *= -1;
-            denominator = Math.abs(denominator);
-         }
       
          output = numerator + "/" + denominator;
       }
       
       if (denominator == 1) { // If the denominator is 1, formats the fraction accordingly.
-      
+         
          output = numerator + "";
       }
-      
       if (numerator == 0) { // If the numerator is 0, formats the fraction accordingly.
-      
+         
          output = whole + "";
       }
-      
-      // return the formatted string
-
       return output;
-      
-   } // end toString
-
-} // end Fraction class
+   }
+}
