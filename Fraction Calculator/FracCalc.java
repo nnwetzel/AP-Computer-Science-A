430e9a5042e81764703de1147ebb8026b24c2a5f
Nat Wetzel <50302468+nnwetzel@users.noreply.github.com>
2021-12-20 16:23:56 -0800

    Finished FracCalc

--- Fraction Calculator/FracCalc.java
+++ Fraction Calculator/FracCalc.java
@@ -1,82 +1,79 @@
 import java.util.Scanner;
 
 public class FracCalc {
-	
+
    public static void main(String[] args) {
-   	
-   	// main variables for the user input and loop control
+
       Scanner input = new Scanner(System.in);
       boolean done = false;
-   
-   	// Instructions
-   	// TODO: Make these instructions your own - making sure to explain what the user needs to do
+
       System.out.println("Welcome to Fraction Calculator.");
       System.out.println("Type \"quit\" to exit the program.\n");
-      
-   	
-   	// Calculator
+
       do {
-      
-      	// Get the input from the user
+
          System.out.println("Please enter an equation that you want solved.");
          System.out.println("For example, \"2_1/3 + 1_1/5\".");
-      
+
          String userInput = input.nextLine();
-      
-         if (userInput.equalsIgnoreCase("quit") )
-         {
+
+         if (userInput.equalsIgnoreCase("quit")) { // Ends program if "quit" is entered.
+
             done = true;
          }
-         else
-         {
+         else {
+
             Fraction result = calculate(userInput);
             System.out.println(result.toString());
          }
-      	
+
       } while (!done);
-   	
+
       input.close();
-   	
    } 
-   	
+
+   /**
+   *  Splits the fraction and solves accordingly. 
+   *  @param - the equation
+   */
    public static Fraction calculate(String input) {
-      
+
       Fraction result = null;
-   
+
       input = input.trim().replaceAll(" +", " ");
-      
-      String[] parts = input.split(" ");
-      
+
+      String[] parts = input.split(" "); // Splits the string into fraction, operand, and fraction.
+
       Fraction frac1 = new Fraction(parts[0]);
       String operand = parts[1];
       Fraction frac2 = new Fraction(parts[2]);
-      
+
       int num = 0;
       int den = 0;
-      
-      if (operand.charAt(0) == '+') {
-         
+
+      if (operand.charAt(0) == '+') { // If the operand is addition, solves the equation accordingly.
+
          num = (frac1.getNum() * frac2.getDen()) + (frac2.getNum() * frac1.getDen());
          den = frac1.getDen() * frac2.getDen();
       }
-      if (operand.charAt(0) == '-') {
-         
+      if (operand.charAt(0) == '-') { // If the operand is subtraction, solves the equation accordingly.
+
          num = (frac1.getNum() * frac2.getDen()) - (frac2.getNum() * frac1.getDen());
          den = frac1.getDen() * frac2.getDen();
       }
-      if (operand.charAt(0) == '*') {
-         
+      if (operand.charAt(0) == '*') { // If the operand is multiplication, solves the equation accordingly.
+
          num = frac1.getNum() * frac2.getNum();
          den = frac1.getDen() * frac2.getDen();
       }
-      if (operand.charAt(0) == '/') {
-         
+      if (operand.charAt(0) == '/') { // If the operand is division, solves the equation accordingly.
+
          num = frac1.getNum() * frac2.getDen();
          den = frac2.getNum() * frac1.getDen();
       }
-      
+
       result = new Fraction(num, den);
-   
+
       return result;
    }
 }
--- Fraction Calculator/Fraction.java
+++ Fraction Calculator/Fraction.java
@@ -1,35 +1,39 @@
 public class Fraction {
-   
+
    private int numerator;
    private int denominator;
 
    /**
-   
-   
+   *  Parses the fraction into numerator and denominator.
+   *  @param - the fraction
    */
    public Fraction(String fraction) {
-   
+
       if (fraction.contains("_") && fraction.contains("/")) { // Parses the fraction if it is a mixed number.
-      
+
          int whole = Integer.parseInt(fraction.substring(0, fraction.indexOf('_')));
          numerator = Integer.parseInt(fraction.substring(fraction.indexOf('_') + 1, fraction.indexOf('/')));
          denominator = Integer.parseInt(fraction.substring(fraction.indexOf('/') + 1));
-         
+
          numerator += Math.abs(whole) * denominator;
-         
+
          if (whole < 0) {
-            
+
             numerator *= -1;
-            
+         }
+         else if (denominator < 0) {
+
+            numerator *= -1;
+            denominator = Math.abs(denominator);
          }
       }
       else if (fraction.contains("/")) { // Parses the fraction.
-         
+
          numerator = Integer.parseInt(fraction.substring(0, fraction.indexOf('/')));
          denominator = Integer.parseInt(fraction.substring(fraction.indexOf('/') + 1));
-         
-         if (numerator > 0 && denominator < 0) {
-            
+
+         if (denominator < 0) {
+
             numerator *= -1;
             denominator = Math.abs(denominator);
          }
@@ -38,76 +42,89 @@ public class Fraction {
          numerator = Integer.parseInt(fraction);
          denominator = 1;
       }
-   
    }
 
+   /**
+   *  Sets the numerator and denominator.
+   *  @param - the numerator
+   *  @param - the denominator
+   */
    public Fraction(int numerator, int denominator) {
-   
+
       this.numerator = numerator;
       this.denominator = denominator;
    }
-  
-   public int getNum() { // Returns numerator.
-   
+
+   /**
+   *   Returns the numerator.
+   */
+   public int getNum() {
+
       return numerator;
    }
-   
-   public int getDen() { // Returns denominator.
-   
+
+   /**
+   *   Returns the numerator.
+   */
+   public int getDen() {
+
       return denominator;
    }
-   
+
+   /**
+   *  Transforms the fraction into a mixed number.
+   */
    public String toString() {
-   
+
       int whole = 0;
       String output = "";
-   
+
       for (int i = Math.abs(numerator); i >= 1; i--) { // Finds the GCD of two fractions and reduces them.
-      
+
          if (Math.abs(numerator) % i == 0 && Math.abs(denominator) % i == 0) {
-         
+
             int gcd = i;
             numerator /= gcd;
             denominator /= gcd;
          }
       }
-   
+
       if (numerator < 0 && denominator < 0) { // Transforms the fraction into a positive fraction if numerator and denominator are negative.
-      
+
          numerator = Math.abs(numerator);
          denominator = Math.abs(denominator);
       }
-      
+
       if (Math.abs(numerator) > Math.abs(denominator)) { // Transforms and formats the fraction into a mixed number if it is improper.
-      
+
          if (numerator < 0 || denominator < 0) { // If the numerator or denominator is negative, transforms the fraction accordingly.
-         
+
             numerator = Math.abs(numerator);
             denominator = Math.abs(denominator);
-         
+
             whole = numerator / denominator;
             numerator %= denominator; 
-         
+
             whole *= -1;
          }
          else {
-         
+
             whole = numerator / denominator;
             numerator %= denominator;
          }
          output = whole + "_" + numerator + "/" + denominator; 
       }
       else if (Math.abs(numerator) < Math.abs(denominator)) { // Formats the fraction if it is not improper.
-      
+
          output = numerator + "/" + denominator;
       }
-      
+
       if (denominator == 1) { // If the denominator is 1, formats the fraction accordingly.
-         
+
          output = numerator + "";
       }
       if (numerator == 0) { // If the numerator is 0, formats the fraction accordingly.
-         
+
          output = whole + "";
       }
       return output;
